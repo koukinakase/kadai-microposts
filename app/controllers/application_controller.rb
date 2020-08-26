bc104d8c0fe275895275a787eb67767ae3493e93
kouki.nakase <yomkh304@gmail.com>
2020-08-26 14:06:05 +0000

    follow, unfollow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,9 @@ class ApplicationController < ActionController::Base
     end
   end
   
-  def count(user)
+  def counts(user)
     @count_microposts = user.microposts.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
   end
 end
