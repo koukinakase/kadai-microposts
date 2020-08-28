fe781c36fde15bf0ca7f23eca84a9880b8413843
kouki.nakase <yomkh304@gmail.com>
2020-08-28 02:27:05 +0000

    like

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,17 @@
+class FavoritesController < ApplicationController
+before_action :require_user_logged_in
+  
+  def create
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.favorite(micropost)
+    flash[:success] = 'お気に入り追加しました。'
+    redirect_to likes_user_path(current_user)
+  end
+
+  def destroy
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.unfavorite(micropost)
+    flash[:success] = 'お気に入り追加を解除しました。'
+    redirect_to likes_user_path(current_user)
+  end
+end
